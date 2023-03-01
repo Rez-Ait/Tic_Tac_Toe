fd2a07b80c2c3302bd7b9bec77dff121ef9aa796
Rez-Ait <61746389+Rez-Ait@users.noreply.github.com>
2023-03-02 00:17:30 +0330

    Add Click To Btn

--- Assets/GameCTRL.cs
+++ Assets/GameCTRL.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class GameCTRL : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameCTRL instance;
+
+    void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    [field: SerializeField]
+    private Sprite shapeXPlayer;
+    [field: SerializeField]
+    private Sprite shapeOPlayer;
+
+    private PlayerType statusPlayer;
+    public PlayerType StatusPlayer { get { return statusPlayer; } }
+
+    private Player firstPlayer;
+    private Player secondPlayer;
+
+    void Start()
     {
-        
+        statusPlayer = PlayerType.XPlayer;
+
+        // Create New 2 Player
+        firstPlayer = new Player(PlayerType.XPlayer, shapeXPlayer);
+        secondPlayer = new Player(PlayerType.OPlayer, shapeOPlayer);
+
     }
+
+    public Player SetClick()
+    {
+
+        switch (statusPlayer)
+        {
+            case PlayerType.XPlayer:
+                statusPlayer = PlayerType.OPlayer;
+                return firstPlayer;
+            case PlayerType.OPlayer:
+                statusPlayer = PlayerType.XPlayer;
+                return secondPlayer;
+        }
+
+        return null;
+
+    }
+
+}
+
+
+public class Player
+{
+    private PlayerType type;
+    private Sprite myShape;
+
+    public Player(PlayerType _type, Sprite _shape)
+    {
+        type = _type;
+        myShape = _shape;
+    }
+
+    public Sprite GetShape()
+    {
+        return myShape;
+    }
+}
+
+public enum PlayerType
+{
+    XPlayer,
+    OPlayer
 }
